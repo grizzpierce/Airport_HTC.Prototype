5c4f87605e3776b69dcd04ef1367f818c3e8906c
Jake Pierce <john.pierce@mymail.champlain.edu>
2016-06-21 18:20:38 -0400

    Oil and probuilder assets

---
+++ Airport_HTC.Prototype/Assets/OilSpilBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OilSpilBehaviour : MonoBehaviour {
+
+    public GameObject m_BubblePrefab;
+    bool m_BubblesCreated = false;
+    
+    void Start()
+    {
+        GameObject newBubbles = (GameObject)Instantiate(m_BubblePrefab, new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z), Quaternion.Euler(new Vector3(-90, 0, 0)));
+    }
+
+	void Update ()
+    {
+        /*
+	    if(this.GetComponent<Animation>().isPlaying == false && m_BubblesCreated == false)
+        {
+            GameObject newBubbles = (GameObject)Instantiate(m_BubblePrefab, new Vector3(transform.position.x, transform.position.y+.1f, transform.position.z), Quaternion.Euler(new Vector3(-90, 0, 0)));
+            m_BubblesCreated = true;
+        }
+        */
+	}
+}
