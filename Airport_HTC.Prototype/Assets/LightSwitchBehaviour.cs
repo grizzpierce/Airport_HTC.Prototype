a30f9d51c4f4a6bdb232b690c08ae709c01f1088
Jake Pierce <john.pierce@mymail.champlain.edu>
2016-06-23 16:02:31 -0400

    Final asset fusebox officially functioning. Bibbits (small and large) prefabs imported.

--- Airport_HTC.Prototype/Assets/LightSwitchBehaviour.cs
+++ Airport_HTC.Prototype/Assets/LightSwitchBehaviour.cs
@@ -3,29 +3,49 @@ using System.Collections;
 
 public class LightSwitchBehaviour : MonoBehaviour {
 
+    public AnimationClip m_OnAnim;
+    public AnimationClip m_OffAnim;
     private VRTK_InteractableObject m_SwitchObj;
-    private Rigidbody m_RB;
-    bool m_AlreadyTouched = false;
+    private Animation m_Anim;
+    private bool m_AlreadyTouched = false;
+    private bool m_LightOn = false;
+
+    public bool GetIfSwitchOn() { return m_LightOn; }
 
     void Start()
     {
         m_SwitchObj = gameObject.GetComponent<VRTK_InteractableObject>();
-        m_RB = gameObject.GetComponent<Rigidbody>();
+        m_Anim = gameObject.GetComponent<Animation>();
     }
 
 
     void Update()
     {
+
         if (m_SwitchObj.IsTouched() && m_AlreadyTouched != true)
         {
             m_AlreadyTouched = true;
-            m_RB.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+
+            Debug.Log("Switch Hit!");
+
+            if (m_LightOn == false)
+            {
+                m_Anim.clip = m_OnAnim;
+                m_Anim.Play();
+                m_LightOn = true;
+            }
+
+            else
+            {
+                m_Anim.clip = m_OffAnim;
+                m_Anim.Play();
+                m_LightOn = false;
+            }
         }
 
-        else
+        else if (m_SwitchObj.IsTouched() != true)
         {
             m_AlreadyTouched = false;
-            m_RB.constraints = RigidbodyConstraints.FreezeAll;
         }
     }
 
