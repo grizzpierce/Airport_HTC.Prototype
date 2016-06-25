ac6b928e1b3f6615885710f625c551c80c14b784
Jake Pierce <john.pierce@mymail.champlain.edu>
2016-06-25 14:08:49 -0400

    Organized project, updated main scene

---
+++ Airport_HTC.Prototype/Assets/Scripts/FuseboxBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuseboxBehaviour : MonoBehaviour {
+
+    GameObject[] m_FuseSwitches;
+    GameObject[] m_OffLights;
+    bool m_LightsOn = false;
+
+
+    // Use this for initialization
+    void Start () {
+        m_FuseSwitches = GameObject.FindGameObjectsWithTag("Light Switch");
+        m_OffLights = GameObject.FindGameObjectsWithTag("Off Light");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (m_LightsOn == false)
+        {
+            bool allLightsOn = true;
+
+            for (int i = 0; i < m_FuseSwitches.Length; ++i)
+            {
+                if(m_FuseSwitches[i].GetComponent<LightSwitchBehaviour>().GetIfSwitchOn() == false)
+                {
+                    Debug.Log("A light is off");
+                    allLightsOn = false;
+                }    
+            }
+
+            if (allLightsOn == true)
+            {
+                m_LightsOn = true;
+            }
+        }
+
+        else
+        {
+            for (int i = 0; i < m_OffLights.Length; ++i)
+            {
+                m_OffLights[i].GetComponent<Light>().enabled = true;
+            }
+        }
+	
+	}
+}
---
+++ Airport_HTC.Prototype/Assets/Scripts/LightSwitchBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitchBehaviour : MonoBehaviour {
+
+    public AnimationClip m_OnAnim;
+    public AnimationClip m_OffAnim;
+    private VRTK_InteractableObject m_SwitchObj;
+    private Animation m_Anim;
+    private bool m_AlreadyTouched = false;
+    private bool m_LightOn = false;
+
+    public bool GetIfSwitchOn() { return m_LightOn; }
+
+    void Start()
+    {
+        m_SwitchObj = gameObject.GetComponent<VRTK_InteractableObject>();
+        m_Anim = gameObject.GetComponent<Animation>();
+    }
+
+
+    void Update()
+    {
+
+        if (m_SwitchObj.IsTouched() && m_AlreadyTouched != true)
+        {
+            m_AlreadyTouched = true;
+
+            Debug.Log("Switch Hit!");
+
+            if (m_LightOn == false)
+            {
+                m_Anim.clip = m_OnAnim;
+                m_Anim.Play();
+                m_LightOn = true;
+            }
+
+            else
+            {
+                m_Anim.clip = m_OffAnim;
+                m_Anim.Play();
+                m_LightOn = false;
+            }
+        }
+
+        else if (m_SwitchObj.IsTouched() != true)
+        {
+            m_AlreadyTouched = false;
+        }
+    }
+
+
+}
