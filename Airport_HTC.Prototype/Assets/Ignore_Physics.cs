deadabdef1c8ae616ac7f5d1b9c487f9f219c766
Jake Pierce <john.pierce@mymail.champlain.edu>
2016-07-25 12:48:48 -0400

    Did a bunch of asset adds to the scene, edited so much stuff. Implemented an ignore by tag component

---
+++ Airport_HTC.Prototype/Assets/Ignore_Physics.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ignore_Physics : MonoBehaviour {
+
+    public List<string> m_IgnoreTags = new List<string>();
+    private int m_IgnoreStage = 0;
+
+	void Awake ()
+    {
+        if (m_IgnoreStage < m_IgnoreTags.Count)
+        {
+            SetIgnore(m_IgnoreTags[m_IgnoreStage]);
+        }
+    }
+
+    public void AddIgnore(string _tag)
+    {
+        m_IgnoreTags.Add(_tag);
+        SetIgnore(_tag);
+    }
+
+    private void SetIgnore(string _tag)
+    {
+        GameObject[] temp = GameObject.FindGameObjectsWithTag(_tag);
+
+        for (int i = 0; i < temp.Length; ++i)
+        {
+            Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), temp[i].GetComponent<Collider>());
+        }
+
+        m_IgnoreStage++;
+    }
+}
