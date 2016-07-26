e7d1b14f81224a8da34eef728309fdd62cb18969
Jake Pierce <john.pierce@mymail.champlain.edu>
2016-07-25 20:05:14 -0400

    Sprucing up the bottom floor with new assets, organized project folder, added lamp swinging

---
+++ Airport_HTC.Prototype/Assets/Scripts/Fusebox/FuseboxBehaviour.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FuseboxBehaviour : MonoBehaviour {
+
+    GameObject[] m_FuseSwitches;
+    public List<GameObject> m_Teleporters = new List<GameObject>();
+    bool m_LightsOn = false;
+    bool m_TurnedOnce = false;
+    bool m_Active = true;
+
+    // SETS IF THE FUSEBOX CAN BE INTERACTED WITH AND SETS IF SWITCHES CAN BE INTERACTED
+    public void SetActive(bool _isActive)
+    {
+        m_Active = _isActive;
+        Debug.Log("FuseBox is active: " + _isActive);
+        if (m_FuseSwitches != null)
+        {
+            for (int i = 0; i < m_FuseSwitches.Length; ++i)
+            {
+                m_FuseSwitches[i].GetComponent<LightSwitchBehaviour>().SetIfLocked(!_isActive);
+            }
+        }
+    }
+
+    void Awake ()
+    {
+        // Stores all fuse switches and teleporters
+        m_FuseSwitches = GameObject.FindGameObjectsWithTag("Light Switch");
+        GameObject[] teleporters = GameObject.FindGameObjectsWithTag("Teleport Point");
+
+        // Checks if any stored teleporters are off
+        for (int i = 0; i < teleporters.Length; ++i)
+        {
+            if(teleporters[i].transform.parent.GetComponent<TeleportShellBehaviour>().IsOffOnStart)
+            {
+                m_Teleporters.Add(teleporters[i].transform.parent.gameObject);
+            }
+        }
+    }
+	
+	void Update ()
+    {
+        if (m_Active)
+        {
+            // Constantly checks if lights are still off
+            if (m_LightsOn == false)
+            {
+                // States they're in the process of turning on
+                bool allLightsOn = true;
+
+                // Checks if all lights are switched up
+                for (int i = 0; i < m_FuseSwitches.Length; ++i)
+                {
+                    // If any aren't, then lights are still off
+                    if (m_FuseSwitches[i].GetComponent<LightSwitchBehaviour>().GetIfSwitchOn() == false)
+                    {
+                        allLightsOn = false;
+                    }
+                }
+
+                // If they end up all being on, then lights are on
+                if (allLightsOn == true)
+                {
+                    m_LightsOn = allLightsOn;
+                }
+            }
+
+            // If the lights are on though
+            else
+            {
+                // And they haven't been turned on once
+                if (!m_TurnedOnce)
+                {
+                    // Then it turns on all lights
+                    for (int i = 0; i < m_Teleporters.Count; ++i)
+                    {
+                        m_Teleporters[i].GetComponent<TeleportShellBehaviour>().IsActive(true);
+                    }
+                    m_TurnedOnce = true;
+                }
+
+            }
+        }
+	}
+}
---
+++ Airport_HTC.Prototype/Assets/Scripts/VinePuller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VinePuller : MonoBehaviour {
+
+    VRTK_InteractableObject m_GrabbableObj;
+    Rigidbody m_RB;
+
+	// Use this for initialization
+	void Start () {
+        m_GrabbableObj = gameObject.GetComponent<VRTK_InteractableObject>();
+        m_RB = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(m_GrabbableObj.IsGrabbed() != true)
+        {
+            m_RB.constraints = RigidbodyConstraints.FreezeAll;
+        }
+
+        else
+        {
+            m_RB.constraints = RigidbodyConstraints.None;
+        }
+	}
+}
