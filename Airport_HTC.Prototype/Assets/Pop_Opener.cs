850b88fa68f6ac799e84a40076830cad3429916b
Jake Pierce <john.pierce@mymail.champlain.edu>
2016-07-27 17:23:41 -0400

    Added audio trigger to pop, added basic dirt blocking deletion, etc.

---
+++ Airport_HTC.Prototype/Assets/Pop_Opener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pop_Opener : MonoBehaviour {
+
+    AudioSource m_Audio;
+    VRTK_InteractableObject m_IntObj;
+    bool m_Opened = false;
+
+    // Use this for initialization
+    void Start () {
+        m_Audio = GetComponent<AudioSource>();
+        m_IntObj = GetComponent<VRTK_InteractableObject>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (m_IntObj.IsGrabbed())
+        {
+            if (!m_Opened)
+            {
+                m_Audio.Play();
+                m_Opened = true;
+            }
+        }
+	}
+}
