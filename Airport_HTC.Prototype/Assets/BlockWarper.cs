74c5f9273d8708eb1731dfb82237f4e4b520efeb
Jake Pierce <john.pierce@mymail.champlain.edu>
2016-08-06 10:51:36 -0400

    Heres the scene, sorry for the startle!

---
+++ Airport_HTC.Prototype/Assets/BlockWarper.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockWarper : MonoBehaviour {
+
+    VRTK_InteractableObject m_IntObj;
+    VRTK_SimplePointer m_CurrentController;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_IntObj = GetComponent<VRTK_InteractableObject>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (m_IntObj.IsGrabbed())
+        {
+            m_CurrentController = m_IntObj.GetGrabbingObject().GetComponent<VRTK_SimplePointer>();
+            m_CurrentController.enabled = false;
+        }
+        else
+        {
+            if(m_CurrentController != null)
+            {
+                m_CurrentController.enabled = true;
+                m_CurrentController = null;
+            }
+        }
+
+	}
+}
