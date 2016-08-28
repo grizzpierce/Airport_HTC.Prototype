2d5fe3c26e9a409261d0b9ddc89838a740b9cd7d
crocodileindie <mr.neil.devine@gmail.com>
2016-08-28 10:10:06 -0400

    trunk animation

---
+++ Airport_HTC.Prototype/Assets/Scripts/Unlocking/TrunkController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrunkController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource m_Audio;
+
+    private bool m_ControllerCollided = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!m_ControllerCollided && col.tag == "Controller")
+        {
+            m_ControllerCollided = true;
+            AudioSource a = GetComponent<AudioSource>();
+            if (a != null)
+            {
+                a.Play();
+            }
+            var anim = gameObject.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.SetTrigger("Trigger_Open");
+            }
+
+        }
+    }
+}
